fcea3fcd561ba27d23fd713fdcae6834a5acd73f
ziheng <shadow1746556951@gmail.com>
2024-06-16 23:08:01 +0800

    feat(posts): 文章列表的移动端适配

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -15,7 +15,7 @@ export default function Posts() {
         '2xl:max-w-6xl xl:max-w-6xl lg:max-w-4xl md:max-w-3xl sm:max-w-2xl',
         'px-8',
         'm-auto',
-        'mt-5  space-y-3'
+        'mt-5 mb-5 space-y-3'
       )}
     >
       {allPostsSorted.map((post: Post) => {
@@ -24,17 +24,17 @@ export default function Posts() {
             href={post.slug}
             key={post._raw.flattenedPath}
             className={cn(
-              'flex',
+              'flex sm:flex-row flex-col',
               'bg-neutral-50',
               'shadow-sm rounded-md',
               'overflow-hidden',
               'p-4'
             )}
           >
-            <div className={cn('mr-5')}>
+            <div className={cn('sm:mr-5 m-0', 'flex-shrink-0')}>
               {post.image && (
                 <Image
-                  className={cn('w-60 h-36', 'rounded-md', 'object-cover')}
+                  className={cn('w-full sm:w-44 md:w-52 lg:w-60  h-36', 'rounded-md', 'object-cover')}
                   src={post.image}
                   alt={'cover'}
                   width={200}
@@ -45,11 +45,19 @@ export default function Posts() {
 
             <div className={cn('flex flex-col justify-between ')}>
               <div>
-                <h1 className={cn('text-xl font-bold leading-loose')}>{post.title}</h1>
-                <p className={cn('text-base text-neutral-500')}>{post.description}</p>
+                <h1 className={cn('sm:text-xl font-bold leading-loose my-2')}>{post.title}</h1>
+                <p
+                  className={cn(
+                    ' sm:text-base text-sm text-neutral-500',
+                    'mb-2',
+                    'text-ellipsis line-clamp-2'
+                  )}
+                >
+                  {post.description}
+                </p>
               </div>
 
-              <div className={cn('text-sm text-neutral-500', 'space-x-4')}>
+              <div className={cn('sm:text-sm text-xs text-neutral-500', 'space-x-4')}>
                 <span>文字数量: {post.readingWords}</span>
                 <span>阅读时长: {Math.floor(post.readingMinutes)} 分钟</span>
               </div>

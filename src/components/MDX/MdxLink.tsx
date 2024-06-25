4e62cb2cc9e053b0c265657f9355a3e3edc91264
ziheng <shadow1746556951@gmail.com>
2024-06-25 14:57:00 +0800

    feat(components / MDX): 增加 MdxLink 组件

---
+++ src/components/MDX/MdxLink.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import Link from 'next/link';
+import { LinkWrap } from '../LinkWrap';
+import { ComponentProps } from 'react';
+
+interface MdxLinkProps extends ComponentProps<typeof Link> {}
+
+export const MdxLink = (props: MdxLinkProps) => {
+  const { href } = props;
+  if (!href) return null;
+
+  return <LinkWrap className=' cursor-pointer' {...props} />;
+};
--- src/components/MDX/index.tsx
+++ src/components/MDX/index.tsx
@@ -1,21 +1,26 @@
 import { cn } from '@/utils';
 import { useMDXComponent } from 'next-contentlayer/hooks';
+import { MdxLink } from './MdxLink';
+import { ComponentProps } from 'react';
 
+type MDXComponentProps = ComponentProps<ReturnType<typeof useMDXComponent>>;
 interface MdxProps {
   code: string;
   className?: string;
 }
+
+const mdxComponents = {
+  a: MdxLink
+} as MDXComponentProps['components'];
 
 /** MDX component */
 export const Mdx = (props: MdxProps) => {
   const { code } = props;
   const Component = useMDXComponent(code);
 
-  const mdxComponents = {};
-
   return (
-    <div className={cn('mdx', props.className)}>
+    <article className={cn('mdx prose', props.className)}>
       <Component components={mdxComponents} />
-    </div>
+    </article>
   );
 };

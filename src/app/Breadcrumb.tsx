e9b888eb88ebf4f95aa76708dbabfda23da7b143
ziheng <shadow1746556951@gmail.com>
2024-07-02 20:05:03 +0800

    pref(Breadcrumb): 文件夹结构优化

--- src/app/Breadcrumb.tsx
+++ src/app/Breadcrumb.tsx
@@ -1,11 +1,58 @@
 'use client';
 
-import { RouterBreadcrumbs } from '@/components/Breadcrumb';
-import { generateNestedPathList, splitPathname } from '@/components/Breadcrumb/utils';
 import { cn } from '@/utils';
 import { usePathname } from 'next/navigation';
 
-export const LayoutBreadcrumb = () => {
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator
+} from '@/components/ui/breadcrumb';
+import React from 'react';
+import { LinkWrap } from '@/components';
+
+import { NestedRouterOption, routerMapping } from '@/config/routerMapping';
+
+/**
+ *  分割路径名称
+ * @param pathname 路径名称
+ * @returns 分割后的路径名称
+ */
+const splitPathname = (pathname: string) => {
+  const pathnameSegments = pathname.split(/\//).filter((v) => v);
+  return pathnameSegments;
+};
+
+/**
+ * 生成嵌套的路径列表
+ * @param pathnameSegments 已分割的路径名称
+ * @returns 嵌套的路径列表
+ */
+const generateNestedPathList = (pathnameSegments: string[]) => {
+  const nested: NestedRouterOption[] = pathnameSegments.map((segment, index) => {
+    const href = `/${pathnameSegments.slice(0, index + 1).join('/')}`;
+
+    return { href, text: segment };
+  });
+
+  return nested;
+};
+
+/**
+ *  根据路由名称获取标题
+ * @param pathname 路由路径
+ * @returns 标题化后的字符串
+ */
+export const titleize = (pathname: string) => {
+  return routerMapping.get(pathname)?.text ?? pathname;
+};
+
+export const LayoutBreadcrumb = (props: { className?: string }) => {
+  const { className } = props;
+
   const pathname = usePathname(); // 获取当前路径
   const pathnameSegments = splitPathname(pathname); // 分割路径
   const nestedPathList = generateNestedPathList(pathnameSegments); // 生成路径列表
@@ -29,7 +76,37 @@ export const LayoutBreadcrumb = () => {
           'flex items-center justify-between flex-1'
         )}
       >
-        <RouterBreadcrumbs items={nestedPathList} />
+        <Breadcrumb className={cn(className)}>
+          <BreadcrumbList className={cn(' flex-nowrap')}>
+            {nestedPathList.map((item, index) => {
+              const isLast = index === nestedPathList.length - 1;
+
+              // 生成面包屑文本 (根据路由或自定义)
+              const text = titleize(item.href) ?? item.text;
+
+              return (
+                <React.Fragment key={item.href}>
+                  {/* 分隔符 */}
+                  {index !== 0 && <BreadcrumbSeparator />}
+
+                  <BreadcrumbItem className={cn('min-w-0')}>
+                    {isLast ? (
+                      // 最后一项不可点击
+                      <BreadcrumbPage className={cn('block truncate')}>{text}</BreadcrumbPage>
+                    ) : (
+                      // 其他项可点击跳转
+                      <BreadcrumbLink className={cn(' whitespace-nowrap')} asChild>
+                        <LinkWrap className={cn('')} href={item.href}>
+                          {text}
+                        </LinkWrap>
+                      </BreadcrumbLink>
+                    )}
+                  </BreadcrumbItem>
+                </React.Fragment>
+              );
+            })}
+          </BreadcrumbList>
+        </Breadcrumb>
       </div>
     </div>
   );
--- src/components/Breadcrumb/type.d.ts
+++
@@ -1,7 +0,0 @@
-import { NestedRouterOption } from '@/config/routerMapping';
-
-export interface RouterBreadcrumbProps {
-  className?: string;
-  generateBreadcrumbText?: (pathname: string) => string | void;
-  items: NestedRouterOption[];
-}

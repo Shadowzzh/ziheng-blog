e26261d34f8f42012241e7f9fce0f35990fde25d
ziheng <shadow1746556951@gmail.com>
2024-07-18 13:51:38 +0800

    pref(components / ThemeProvider): 切换主题实现方式优化

--- src/components/ThemeProvider/ThemeToggle.tsx
+++ src/components/ThemeProvider/ThemeToggle.tsx
@@ -1,7 +1,9 @@
 'use client';
 
 import { useTheme } from 'next-themes';
-import { GiEvilMoon, GiStripedSun } from 'react-icons/gi';
+import { RiMoonFoggyLine } from '@react-icons/all-files/ri/RiMoonFoggyLine';
+import { RiSunFoggyLine } from '@react-icons/all-files/ri/RiSunFoggyLine';
+
 import { Button } from '../ui/button';
 import { cn } from '@/utils';
 
@@ -10,21 +12,39 @@ interface ModeToggleProps {
 }
 
 export function ModeToggle(props: ModeToggleProps) {
-  const { setTheme, theme } = useTheme();
-
-  const isDark = theme !== 'light'; // !== 'light' 保证了默认是 dark
+  const { setTheme } = useTheme();
 
   return (
-    <div className={cn(props.className)}>
+    <div className={cn('relative', 'group', 'md:hover:animate-pulse', props.className)}>
       <Button
         aria-label='toggle theme'
+        className={cn('visible dark:invisible')}
         size={'icon'}
         variant={'ghost'}
-        onClick={() => setTheme(isDark ? 'light' : 'dark')}
+        onClick={() => setTheme('dark')}
       >
-        <div className={cn()}>
-          {isDark ? <GiEvilMoon className={'size-6'} /> : <GiStripedSun className={'size-6'} />}
-        </div>
+        <RiSunFoggyLine
+          className={cn(
+            'size-5 ',
+            'transition-all duration-700 ease-in-out',
+            'scale-100 group-hover:scale-110'
+          )}
+        />
+      </Button>
+      <Button
+        aria-label='toggle theme'
+        className={cn('invisible dark:visible', 'absolute top-0 left-0')}
+        size={'icon'}
+        variant={'ghost'}
+        onClick={() => setTheme('light')}
+      >
+        <RiMoonFoggyLine
+          className={cn(
+            'size-5',
+            'transition-all duration-700 ease-in-out',
+            'scale-100 group-hover:scale-110'
+          )}
+        />
       </Button>
     </div>
   );

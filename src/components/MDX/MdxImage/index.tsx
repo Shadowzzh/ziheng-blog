11f4bf19cf185876727ecb7f94dad11526dcb778
ziheng <shadow1746556951@gmail.com>
2024-09-25 19:19:28 +0800

    feat(MdxImage): 预览组件使用 popoper 组件

--- src/components/MDX/MdxImage/index.tsx
+++ src/components/MDX/MdxImage/index.tsx
@@ -4,12 +4,20 @@ import type { ImageProps } from 'next/image';
 
 import { PreviewImage } from './PreviewImage';
 import { ServerGenBlurDataURL } from '@/components/ImageWrap';
+import { cn } from '@/utils';
+import { omit } from '@/lib/utils';
 
 interface MdxImageProps extends ImageProps {}
 
 export const MdxImage = async (props: MdxImageProps) => {
   return ServerGenBlurDataURL({
     src: props.src,
-    children: (params) => <PreviewImage blurDataURL={params.blurDataURL} {...props} />
+    children: (params) => (
+      <PreviewImage
+        className={cn(props.className)}
+        blurDataURL={params.blurDataURL}
+        {...omit(props, ['className'])}
+      />
+    )
   });
 };

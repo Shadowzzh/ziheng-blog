378d287ab376b630e535bf60d57816e9eec969b7
ziheng <shadow1746556951@gmail.com>
2024-09-26 14:31:01 +0800

    feat(app / feed): 更新 feed 配置信息

--- src/app/feed.xml/route.ts
+++ src/app/feed.xml/route.ts
@@ -3,38 +3,38 @@ import { mdxToHtml } from '@/lib/mdxToHtml';
 import { allPosts } from 'contentlayer/generated';
 import { Feed } from 'feed';
 
+const feedOptions = {
+  title: 'Hi,Zane',
+  description: '我对产品、技术和运动充满热情，运用编码技能实现自己的想法。',
+  id: WEB_SITE_URL,
+  link: WEB_SITE_URL,
+  language: 'zh-CN',
+  image: `${WEB_SITE_URL}/logo.png`,
+  favicon: `${WEB_SITE_URL}/favicon.ico`,
+  copyright: `All rights reserved ${new Date().getFullYear()}, Zane`,
+  updated: new Date(),
+  generator: 'Feed for https://github.com/jpmonette/feed',
+  feedLinks: {
+    rss2: `${WEB_SITE_URL}/feed.xml`,
+    json: `${WEB_SITE_URL}/feed.json`,
+    atom: `${WEB_SITE_URL}/atom.xml`
+  },
+  author: {
+    name: AUTHOR.NAME,
+    email: AUTHOR.EMAIL,
+    link: AUTHOR.TWITTER
+  },
+  follow_challenge: {
+    feed_id: '57428565962095616',
+    user_id: '41448350114993152'
+  }
+};
+
 /**
  * 生成 feed.xml
  * @returns  feed.xml
  */
 export const GET = async () => {
-  const feedOptions = {
-    title: 'Hi , Zane',
-    description: 'Latest articles on web development and technology',
-    id: WEB_SITE_URL,
-    link: WEB_SITE_URL,
-    language: 'zh-CN',
-    image: `${WEB_SITE_URL}/logo.png`,
-    favicon: `${WEB_SITE_URL}/favicon.ico`,
-    copyright: `All rights reserved ${new Date().getFullYear()}, Zane`,
-    updated: new Date(),
-    generator: 'Feed for Node.js',
-    feedLinks: {
-      rss2: `${WEB_SITE_URL}/feed.xml`,
-      json: `${WEB_SITE_URL}/feed.json`,
-      atom: `${WEB_SITE_URL}/atom.xml`
-    },
-    author: {
-      name: AUTHOR.NAME,
-      email: AUTHOR.EMAIL,
-      link: AUTHOR.TWITTER
-    },
-    follow_challenge: {
-      feed_id: '57428565962095616',
-      user_id: '41448350114993152'
-    }
-  };
-
   const feed = new Feed(feedOptions);
 
   await Promise.all(

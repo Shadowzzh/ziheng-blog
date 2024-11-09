9bbccac170495b735f0d708115786f2c844842a1
ziheng <shadow1746556951@gmail.com>
2024-11-09 09:30:11 +0800

    chore(config): 只在开发环境显示相册

--- src/config/nav.tsx
+++ src/config/nav.tsx
@@ -4,6 +4,6 @@ export const HOME_NAV = { href: '/', text: '首页' };
 export const navigationItems: NestedRouterOption[] = [
   HOME_NAV,
   { href: '/posts', text: '文章' },
-  { href: '/gallery', text: '相册' },
+  process.env.NODE_ENV === 'development' ? { href: '/gallery', text: '相册' } : null,
   { href: '/about', text: '关于' }
-];
+].filter(Boolean) as NestedRouterOption[];
